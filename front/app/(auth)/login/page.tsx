304fed784787fb7ae891d08d2d64f8e869beac74
lauramussa <lauramussa94@gmail.com>
2025-10-23 15:08:29 -0300

    se unificaron estilos de formularios

--- front/app/(auth)/login/page.tsx
+++ front/app/(auth)/login/page.tsx
@@ -1,7 +1,95 @@
 
-
+import Image from "next/image";
+import Link from "next/link";
 const page = () => {
-  return <div>este es mi login</div>;
+  return (
+    <div className="min-h-screen text-font-light flex flex-col">
+      <header className="p-4">
+        <Link href="/">
+          <span className="font-semibold">LOGO</span>
+        </Link>
+      </header>
+
+      <section className="flex flex-1 justify-center items-center px-4">
+        <form className="border-border border p-8 rounded-2xl w-full max-w-lg shadow-lg m-15">
+          <h1 className="text-4xl font-bold text-center mb-2">Login</h1>
+          <p className="text-gray-400 text-center mb-6">
+            Iniciá sesión para ingresar a tu cuenta.
+          </p>
+
+          <div className="flex flex-col gap-4">
+            <div>
+              <label htmlFor="email" className="block text-sm mb-1">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Ingresá tu email"
+                className="w-full h-12 rounded-md bg-background2 px-3 text-sm focus:outline-none focus:ring-1 focus:ring-purple-300/50"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="password" className="block text-sm mb-1">
+                Contraseña
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Creá tu contraseña"
+                className="w-full h-12 rounded-md bg-background2 px-3 text-sm focus:outline-none focus:ring-1 focus:ring-purple-300/50"
+              />
+            </div>
+
+            <div className="flex items-center gap-2 text-xs text-gray-300">
+              <label className="inline-flex items-center cursor-pointer ">
+                <input type="checkbox" className="sr-only" />
+
+                <div className="w-5 h-5 border border-border rounded-[5px] flex items-center justify-center">
+                  <div className="w-3 h-2.5 bg-accent-dark rounded-xs hidden checkbox-indicator"></div>
+                </div>
+                <label htmlFor="terms" className="ml-2 select-none text-sm ">
+                  Recordarme
+                </label>
+              </label>
+            </div>
+
+            <button
+              type="submit"
+              className="bg-button/90 hover:bg-button cursor-pointer transition rounded-md py-2 mt-2 font-semibold"
+            >
+              Iniciar sesión
+            </button>
+
+            <div className="flex items-center my-2">
+              <div className="flex-1 h-px bg-gray-medium-dark"></div>
+              <span className="px-2 text-gray-medium-light text-xl">o</span>
+              <div className="flex-1 h-px bg-gray-medium-dark"></div>
+            </div>
+
+            <button className="flex items-center justify-center gap-2 bg-font-light cursor-pointer text-font-dark py-2 rounded-md hover:bg-gray-100 transition">
+              <Image
+                src="/icons/googleIcon.svg"
+                width={18}
+                height={18}
+                alt="Ícono de Google"
+              />
+              Ingresá con Google
+            </button>
+
+            <p className="text-center text-gray-400 text-sm mt-2">
+              ¿Todavía no tenés una cuenta?{" "}
+              <Link href="/register" className="text-accent-medium underline">
+                Registrate
+              </Link>
+              <span className="items-center text-xl">&rarr;</span>
+            </p>
+          </div>
+        </form>
+      </section>
+    </div>
+  );
 };
 
 export default page;

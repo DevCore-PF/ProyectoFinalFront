4d81b34bc8079c643c3a5254b5146993c10ccd37
AdanLFranco <adanleofranco@gmail.com>
2025-10-26 21:56:46 -0300

    coursecard partially done

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,9 +1,17 @@
+import CourseCard from "@/components/CourseCard";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main>
-      <HeroSection />
-    </main>
+    <>
+      <Navbar />
+      <main className="flex flex-col items-center gap-4">
+        <HeroSection />
+        <CourseCard />
+      </main>
+      <Footer />
+    </>
   );
 }

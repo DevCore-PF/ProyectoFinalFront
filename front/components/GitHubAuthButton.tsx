f619aa6a8bb7039d23d49e6e0e325149ec340e12
lauramussa <lauramussa94@gmail.com>
2025-10-31 17:24:38 -0300

    se modifico la autenticacion con github

--- front/components/GitHubAuthButton.tsx
+++ front/components/GitHubAuthButton.tsx
@@ -1,18 +1,26 @@
 import { FaGithub } from "react-icons/fa";
 
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const GitHubAuthButton = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const handleGitHubAuth = () => {
+    setIsLoading(true);
     window.location.href = `${API_URL}/auth/github`;
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
   };
+
   const pathname = usePathname();
 
   return (
     <button
       type="button"
       onClick={handleGitHubAuth}
+      disabled={isLoading}
       className="flex items-center cursor-pointer disabled:cursor-not-allowed disabled:opacity-50 justify-center gap-2 bg-slate-900 text-font-light py-2 rounded-md hover:bg-slate-900/40 transition text-xs sm:text-sm px-10 sm:px-4"
     >
       <FaGithub size={30} />

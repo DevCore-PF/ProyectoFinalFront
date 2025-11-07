d02763adb62b03ce05266ad79b552a18e7e7c227
lauramussa <lauramussa94@gmail.com>
2025-11-07 13:34:17 -0300

    Se completo el build para el deploy, pagina 100% funcional

--- front/app/loader/page.tsx
+++ front/app/loader/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 import Loader from "@/components/Loaders/Loader";
-import Spinner from "@/components/Loaders/TinyLoader2";
-import React from "react";
+
 const page = () => {
   return (
     <>
       <Loader />
-      {/* <Spinner/> */}
     </>
   );
 };

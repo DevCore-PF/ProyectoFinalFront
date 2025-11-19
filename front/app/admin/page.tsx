cc79fe1b33ebeb0971fc5f73e62de47640e7eb14
lauramussa <lauramussa94@gmail.com>
2025-11-18 23:26:05 -0300

    Ahora si se soluciono admin en mobile

--- front/app/admin/page.tsx
+++ front/app/admin/page.tsx
@@ -29,12 +29,9 @@ import ProfileValidationDetails from "@/components/admin/ProfileValidationDetail
 import NewsletterManagement from "@/components/admin/NewsletterManagement";
 
 type ValidationType = "professor" | "course_create" | "course_edit" | "profile_edit" | "role_change";
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+
 const AdminDashboard = () => {
   const [activeTab, setActiveTab] = useState<TabType>("validations");
-  const [searchTerm, setSearchTerm] = useState("");
-  const [filterStatus, setFilterStatus] = useState<string>("all");
   const [selectedValidation, setSelectedValidation] = useState<ValidationRequest | null>(null);
   const [showBar, setShowBar] = useState(true);
 
